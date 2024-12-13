db099cca6cfd20a5d2a3a4cba8e40aa4746f99c1
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-13 22:49:32 +0545

    complete dark mode functionality

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,12 +1,15 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const {theme} = useSelector((state) => state.theme);
   return (
     <Navbar className="border-b-2">
       <Link
@@ -30,8 +33,13 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray">
-          <FaMoon />
+        <Button
+          onClick={() => dispatch(toggleTheme())}
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+        >
+          {theme === "light" ? <FaSun /> : <FaMoon />}
+         
         </Button>
         {currentUser ? (
           <Dropdown
@@ -47,25 +55,19 @@ export default function Header() {
                 {currentUser.email}
               </span>
             </Dropdown.Header>
-            <Link to={'/dashboard?tab=profile'}>
-            <Dropdown.Item>
-              Profile
-            </Dropdown.Item>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-          
-            <Dropdown.Item>
-             Sign Out
-            </Dropdown.Item>
-           
-           
+
+            <Dropdown.Item>Sign Out</Dropdown.Item>
           </Dropdown>
         ) : (
-          <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>
-            Sign In
-          </Button>
-        </Link>
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
         )}
         <Link to="/sign-in">
           <Button gradientDuoTone="purpleToBlue" outline>
