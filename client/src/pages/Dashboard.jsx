cc31bad3667890540ebd151ddf0a7a72c79511ef
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-13 23:23:08 +0545

    complete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../component/DashSidebar";
+import DashProfile from "../component/DashProfile";
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    // console.log(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>
-      dashboard
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile */}
+      {tab === "profile" && <DashProfile />}
     </div>
-  )
+  );
 }
